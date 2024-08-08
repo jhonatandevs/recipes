365c47b56558834df378c17e66bdc35910ecd071
JhonatanDev <jhonatan3506@gmail.com>
2024-08-08 18:56:33 -0500

    detail page

--- src/app/dashboard/(content)/detail/[id]/page.tsx
+++ src/app/dashboard/(content)/detail/[id]/page.tsx
@@ -1,30 +1,49 @@
 import { notFound } from "next/navigation";
-import { RecipesResponse } from "../../home/interfaces/recipes";
+import { Recipe, RecipesResponse } from "../../home/interfaces/recipes";
+import TableDetailRecipe from "@/components/recipes/table";
+import Link from "next/link";
+import { IoArrowBack } from "react-icons/io5";
 
 interface Props {
   params: { id: string }
 }
-const getSingleRecipes = async (id = '1'): Promise<any> => {
+const getSingleRecipes = async (id = '1'): Promise<Recipe | undefined> => {
+  console.log("get simple")
   try {
-    const data: any = await fetch(`'https://dummyjson.com/recipes/'${id}`, {
-      cache: 'force-cache', //TODO: CAMBIAR ESTO EN FUTURO,
-      next: {
-        revalidate: 60 * 60 * 30 * 6
-      }
-    })
+    const data: Recipe = await fetch(`https://dummyjson.com/recipes/${id}`)
       .then(res => res.json());
+    console.log("Respuesta:  ", data)
     return data;
   }
   catch (error) {
-    notFound()
+    notFound();
   }
 }
 export default async function DetailPage({ params }: Props) {
-  const recipes = await getSingleRecipes(params.id);
+  const recipe = await getSingleRecipes(params.id);
 
   return (
-    <div>
-      <h1>Hello Page</h1>
-    </div>
+    <section className=" bg-blueGray-50">
+      <Link href={'/dashboard/home'}>
+        <p className='flex items-center text-blue-900 pt-5'> 
+          <span className='pr-1 items-center'><IoArrowBack/> </span>
+          Volver
+          </p>
+        </Link>
+      <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-4">
+        <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
+          <div className="rounded-t mb-0 px-4 py-3 border-0">
+            <div className="flex flex-wrap items-center">
+              <div className="relative w-full px-4 max-w-full flex-grow flex-1">
+                <h3 className="font-semibold text-center text-blueGray-700">Detalle Recipe </h3>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        
+        <TableDetailRecipe recipe={recipe} />
+      </div>
+    </section>
   );
 }
---
+++ src/components/recipes/table.tsx
@@ -0,0 +1,77 @@
+import { Recipe } from '@/app/dashboard/(content)/home/interfaces/recipes'
+import Link from 'next/link'
+import React from 'react'
+import { IoArrowBack } from 'react-icons/io5'
+interface Props {
+    recipe?: Recipe
+}
+export default function TableDetailRecipe({ recipe }: Props) {
+    return (
+
+        <>
+            <div>
+           <div className="w-10">
+         
+           </div>
+                <div className="block w-4/5  mt-3 bg-white mx-auto ">
+                    <table className="items-center bg-transparent w-full border-collapse">
+                        <thead>
+                            <tr>
+                                <th colSpan={4} className="text-center px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                    {recipe?.name} <small>[{recipe?.id}]</small>
+                                </th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-center text-blueGray-700 ">
+                                    Ingredientes
+                                </th>
+                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 max-w-40">
+                                    {recipe?.ingredients.map((ingredient: string, index: number) => {
+                                        return (
+                                            <p key={'ingrediet-' + index}>- {ingredient}</p>
+                                        )
+                                    })}
+                                </td>
+                            </tr>
+                            <tr>
+                                <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-center text-blueGray-700 ">
+                                    Instrucciones
+                                </th>
+                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-normal p-4 w-full">
+                                    {recipe?.instructions.map((instruction: string, index: number) => {
+                                        return (
+                                            <p key={'instruction-' + index}>- {instruction}</p>
+                                        )
+                                    })}
+                                </td>
+                            </tr>
+                            <tr>
+                                <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-center text-blueGray-700 ">
+                                    Valoraciones
+                                </th>
+                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
+                                    {recipe?.rating}
+                                </td>
+                            </tr>
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
+            <footer className="relative pt-8 pb-6 mt-16">
+                <div className="container mx-auto px-4">
+                    <div className="flex flex-wrap items-center md:justify-between justify-center">
+                        <div className="w-full md:w-6/12 px-4 mx-auto text-center">
+                            <div className="text-sm text-blueGray-500 font-semibold py-1">
+                                Jhonatan Zuniga 2024
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer></>
+
+    )
+}
